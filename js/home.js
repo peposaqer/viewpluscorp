df999a6fd0feecb3fe6e3e77702d129b9af282bc
Abdallah Saqer <peposaqer@outlook.com>
2020-11-05 10:28:04 +0200

    Edit side-menu

--- js/home.js
+++ js/home.js
@@ -1,38 +1,36 @@
 $("nav .x-mark").on("click", function () {
-    $("nav ul").hide();
-    $("#check").prop("checked", false);
+  $("nav ul").hide();
+  $("#check").prop("checked", false);
 });
 
 $(".open-nav").on("click", function () {
-    $("nav ul").css("display", "block");
+  $("nav ul").css("display", "block");
 });
 
-
-  $(".customer-logos").slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false,
-    dots: false,
-    pauseOnHover: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4,
-        },
+$(".customer-logos").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrows: false,
+  dots: false,
+  pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
       },
-      {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 3,
-        },
+    },
+    {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3,
       },
-    ],
-  });
+    },
+  ],
+});
 
-  
 $(document).ready(function () {
   AOS.init({
     // Global settings:
@@ -55,3 +53,80 @@ $(document).ready(function () {
     anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
   });
 });
+
+// // hide navbar in footer only
+// $(document).ready(function () {
+//   // $(".navbar").hide(); //Hide the navigation bar first
+//   $(window).scroll(function () {
+//     //Listen for the window's scroll event
+//     if (isScrolledAfterElement("#footer")) {
+//       //if it has scrolled beyond the #content elment
+//       $("nav").fadeOut(); //Show the navigation bar
+//     } else {
+//       $("nav").fadeIn(); //Else hide it
+//     }
+//   });
+
+//   //Function that returns true if the window has scrolled beyond the given element
+//   function isScrolledAfterElement(elem) {
+//     var $elem = $(elem);
+//     var $window = $(window);
+//     var docViewTop = $window.scrollTop();
+//     var docViewBottom = docViewTop + $window.height();
+//     var elemTop = $elem.offset().top;
+
+//     return elemTop <= docViewBottom;
+//   }
+// });
+
+$("#smart-nav").css("visibility", "collapse");
+$("#Evaluation-nav").css("visibility", "collapse");
+$("#Veyolo-nav").css("visibility", "collapse");
+$("#inon-nav").css("visibility", "collapse");
+
+defaultOffset = 250;
+
+document.onscroll = function () {
+  if (
+    window.scrollY >= $("#smart").offset().top - defaultOffset &&
+    window.scrollY <=
+      $("#smart").offset().top + $("#smart").height() - defaultOffset
+  ) {
+    $("#smart-nav").css("visibility", "visible");
+    $("#Evaluation-nav").css("visibility", "collapse");
+    $("#Veyolo-nav").css("visibility", "collapse");
+    $("#inon-nav").css("visibility", "collapse");
+  } else if (
+    window.scrollY >= $("#Evaluation").offset().top - defaultOffset &&
+    window.scrollY <=
+      $("#Evaluation").offset().top + $("#Evaluation").height() - defaultOffset
+  ) {
+    $("#smart-nav").css("visibility", "collapse");
+    $("#Evaluation-nav").css("visibility", "visible");
+    $("#Veyolo-nav").css("visibility", "collapse");
+    $("#inon-nav").css("visibility", "collapse");
+  } else if (
+    window.scrollY >= $("#Veyolo").offset().top - defaultOffset &&
+    window.scrollY <=
+      $("#Veyolo").offset().top + $("#Veyolo").height() - defaultOffset
+  ) {
+    $("#smart-nav").css("visibility", "collapse");
+    $("#Evaluation-nav").css("visibility", "collapse");
+    $("#Veyolo-nav").css("visibility", "visible");
+    $("#inon-nav").css("visibility", "collapse");
+  } else if (
+    window.scrollY >= $("#inon").offset().top - defaultOffset &&
+    window.scrollY <=
+      $("#inon").offset().top + $("#inon").height() - defaultOffset
+  ) {
+    $("#smart-nav").css("visibility", "collapse");
+    $("#Evaluation-nav").css("visibility", "collapse");
+    $("#Veyolo-nav").css("visibility", "collapse");
+    $("#inon-nav").css("visibility", "visible");
+  } else {
+    $("#smart-nav").css("visibility", "collapse");
+    $("#Evaluation-nav").css("visibility", "collapse");
+    $("#Veyolo-nav").css("visibility", "collapse");
+    $("#inon-nav").css("visibility", "collapse");
+  }
+};
